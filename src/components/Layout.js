d2e07ede6702f2ac00d013908f26c09a411828d7
David OH <holifeilddavid@gmail.com>
2021-08-05 21:25:22 +0300

    Avatars Used and styled

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,32 +1,53 @@
 import React from "react";
 import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { ListItemIcon, makeStyles } from "@material-ui/core";
+import { Avatar, ListItemIcon, makeStyles } from "@material-ui/core";
 import { AddCircleOutlineOutlined, SubjectOutlined } from "@material-ui/icons";
 import { useHistory, useLocation } from "react-router-dom";
 
+import { format } from "date-fns";
+
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-  },
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-  },
-  drawer: {
-    width: drawerWidth,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  active: {
-    background: "#f4f4f4",
-  },
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      display: "flex",
+      padding: theme.spacing(3),
+    },
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+    toolbar: theme.mixins.toolbar,
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    date: {
+      flexGrow: 1,
+    },
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
+  };
 });
 
 function Layout({ children }) {
@@ -49,6 +70,15 @@ function Layout({ children }) {
 
   return (
     <div className={classes.root}>
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar>
+          <Typography className={classes.date}>
+            Today is the {format(new Date(), "do MMMM Y")}
+          </Typography>
+          <Typography>Ned-ler</Typography>
+          <Avatar className={classes.avatar} src="/rimuru.jpg" />
+        </Toolbar>
+      </AppBar>
       {/* Sidebar Drawer */}
       <Drawer
         className={classes.drawer}
@@ -57,7 +87,9 @@ function Layout({ children }) {
         classes={{ paper: classes.drawerPaper }}
       >
         <div>
-          <Typography variant="h5">Ned-ler</Typography>
+          <Typography variant="h5" className={classes.title}>
+            Ned-ler
+          </Typography>
         </div>
 
         {/* List of links */}
@@ -77,7 +109,10 @@ function Layout({ children }) {
           ))}
         </List>
       </Drawer>
-      <div className={classes.page}>{children}</div>
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
     </div>
   );
 }
